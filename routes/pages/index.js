c2d0d8e0e976c082f4b4a21748bc413bf53f0450
gongls <imgongls@sina.com>
2016-05-24 13:36:20 +0800

    up

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -16,7 +16,19 @@ router.use('/blog', blog);
 router.use('/h5', h5);
 router.use('/robot', robot);
 router.get('/', function(req, res, next) {
-  res.render('index',{title:'index',js:'index'});
+  res.render('home',{title:'我喜欢的一切',subTitle:'愿一切喜欢的都在一起',js:'clean-blog'});
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact',{title:'我喜欢的一切',subTitle:'愿一切喜欢的都在一起',js:'clean-blog'});
+});
+router.get('/post', function(req, res, next) {
+  res.render('post',{title:'我喜欢的一切',subTitle:'愿一切喜欢的都在一起',js:'clean-blog'});
+});
+router.get('/login', function(req, res, next) {
+  res.render('login',{title:'login',js:'login'});
+});
+router.get('/mp', function(req, res, next) {
+  res.render('mp/index',{title:'mp',js:'mp'});
 });
 //router.use('/disease', auth, disease);
 
